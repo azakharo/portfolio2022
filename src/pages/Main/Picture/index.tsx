c3b0a5751d66813079f9e18da2fe53550e5bba14
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2022-07-15 16:04:06 +0300

    added hero image

--- src/pages/Main/Picture/index.tsx
+++ src/pages/Main/Picture/index.tsx
@@ -1,17 +1,43 @@
 import React, { FC, memo } from 'react';
-import { Box, makeStyles } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    minHeight: 400,
-    backgroundColor: theme.palette.primary.main,
+import portraitImage from 'src/assets/portrait.jpg';
+
+const useStyles = makeStyles(() => ({
+  imageContainer: {
+    // Use "linear-gradient" to add darken background effect to the image. This will make the text easier to read
+    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), url("${portraitImage}")`,
+    // Set a specific height
+    height: '50vh',
+
+    // Position and center the image to scale nicely on all screens
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    position: 'relative',
+  },
+  text: {
+    textAlign: 'center',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    color: 'white',
   },
 }));
 
 const Picture: FC = () => {
   const classes = useStyles();
 
-  return <Box className={classes.root}>Picture</Box>;
+  return (
+    <div className={classes.imageContainer}>
+      <div className={classes.text}>
+        <h1>I am Alexey Zakharov</h1>
+        <p>And I am a Programmer</p>
+        <span>Hire me</span>
+      </div>
+    </div>
+  );
 };
 
 export default memo(Picture);
