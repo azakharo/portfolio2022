8daf06809f60b0dac0fe14303d3a4ce43cb13144
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2022-07-16 13:09:37 +0300

    implemented the about section

--- src/pages/Main/About/index.tsx
+++ src/pages/Main/About/index.tsx
@@ -1,17 +1,33 @@
 import React, { FC, memo } from 'react';
-import { Box, makeStyles } from '@material-ui/core';
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    minHeight: 400,
-    backgroundColor: theme.palette.secondary.main,
-  },
-}));
+import { Box, Container, Typography } from '@material-ui/core';
+import { useTranslation } from 'react-i18next';
 
 const About: FC = () => {
-  const classes = useStyles();
+  const [t] = useTranslation();
 
-  return <Box className={classes.root}>About</Box>;
+  return (
+    <Container maxWidth="lg">
+      <Box display="flex" p={4} gridGap={40}>
+        <Box flex={1} textAlign="center">
+          <Typography variant="h2" color="textPrimary">
+            {t('about__title', 'Привет!')}
+          </Typography>
+          <Typography variant="subtitle1" color="textSecondary">
+            {t('about__subTitle', 'немного о себе...')}
+          </Typography>
+        </Box>
+        <Box flex={2}>
+          <Typography variant="body1" color="textPrimary">
+            {t('about__whereIlive')}
+          </Typography>
+          <br />
+          <Typography variant="body1" color="textPrimary">
+            {t('about__myInterests')}
+          </Typography>
+        </Box>
+      </Box>
+    </Container>
+  );
 };
 
 export default memo(About);
