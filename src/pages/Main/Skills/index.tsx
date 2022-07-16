a1678381fad0d53b20d19192c67444543f170f31
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2022-07-16 15:48:19 +0300

    skills: impl the skeleton

--- src/pages/Main/Skills/index.tsx
+++ src/pages/Main/Skills/index.tsx
@@ -1,5 +1,40 @@
 import React, { FC, memo } from 'react';
-import { Box, makeStyles } from '@material-ui/core';
+import { Box, Container, LinearProgress, makeStyles } from '@material-ui/core';
+
+const skills = [
+  {
+    name: 'Javascript, ES6, 7, 8... Babel, Webpack',
+    value: 90,
+  },
+  {
+    name: 'React.js, Redux, redux-thunk, redux-saga',
+    value: 85,
+  },
+  {
+    name: 'Vue, vuex',
+    value: 65,
+  },
+  {
+    name: 'AngularJS',
+    value: 75,
+  },
+  {
+    name: 'HTML, Pug',
+    value: 80,
+  },
+  {
+    name: 'CSS, Less, PostCSS',
+    value: 80,
+  },
+  {
+    name: 'Responsive layout, Flexbox, CSS Grid',
+    value: 85,
+  },
+  {
+    name: 'Node.js, Express.js, MongoDB',
+    value: 75,
+  },
+];
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -11,7 +46,29 @@ const useStyles = makeStyles(theme => ({
 const Skills: FC = () => {
   const classes = useStyles();
 
-  return <Box className={classes.root}>Skills</Box>;
+  // This component repeats the layout of the About comp.
+  return (
+    <Container maxWidth="lg">
+      <Box display="flex" p={4} gridGap={40}>
+        <Box flex={1} />
+        <Box flex={2}>
+          {skills.map(({ name, value }) => (
+            <Box key={name} mb={2}>
+              <Box mb={1} display="flex" justifyContent="space-between">
+                <span>{name}</span>
+                <span>{value}%</span>
+              </Box>
+              <LinearProgress
+                variant="determinate"
+                color="secondary"
+                value={value}
+              />
+            </Box>
+          ))}
+        </Box>
+      </Box>
+    </Container>
+  );
 };
 
 export default memo(Skills);
