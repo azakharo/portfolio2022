29de7ded47f1f9d24914099135fe2d1b832f1786
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2022-07-16 16:33:21 +0300

    skills: impl the animation

--- src/pages/Main/Skills/Progress.tsx
+++ src/pages/Main/Skills/Progress.tsx
@@ -1,5 +1,7 @@
-import React, { FC, memo } from 'react';
+import random from 'lodash/random';
+import React, { FC, memo, useMemo, useState } from 'react';
 import { Box, LinearProgress } from '@material-ui/core';
+import useInterval from 'ahooks/lib/useInterval';
 
 // const useStyles = makeStyles(theme => ({
 //   root: {},
@@ -10,16 +12,34 @@ interface Props {
   value: number;
 }
 
-const Progress: FC<Props> = ({ label, value }) => (
+const Progress: FC<Props> = ({ label, value }) => {
+  const interval = useMemo(() => random(1, 5, false) * 10, []); // ms
+
+  const [currentValue, setCurrentValue] = useState(0);
+  const clearInterval = useInterval(() => {
+    if (currentValue < value) {
+      setCurrentValue(currentValue + 1);
+    } else {
+      clearInterval();
+    }
+  }, interval);
+
   // const classes = useStyles();
 
   // This component repeats the layout of the About comp.
-  <>
-    <Box mb={1} display="flex" justifyContent="space-between">
-      <span>{label}</span>
-      <span>{value}%</span>
-    </Box>
-    <LinearProgress variant="determinate" color="secondary" value={value} />
-  </>
-);
+  return (
+    <>
+      <Box mb={1} display="flex" justifyContent="space-between">
+        <span>{label}</span>
+        <span>{currentValue}%</span>
+      </Box>
+      <LinearProgress
+        variant="determinate"
+        color="secondary"
+        value={currentValue}
+      />
+    </>
+  );
+};
+
 export default memo(Progress);
