0368da19634cca243b2f3a052a63edd52dab5677
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2022-10-14 18:09:05 +0300

    example popup: impl content

--- src/pages/Main/Examples/PopUp.tsx
+++ src/pages/Main/Examples/PopUp.tsx
@@ -1,5 +1,13 @@
+import capitalize from 'lodash/capitalize';
 import React, { FC, memo, useCallback } from 'react';
-import { DialogContent, Dialog } from '@material-ui/core';
+import {
+  DialogContent,
+  Dialog,
+  Box,
+  makeStyles,
+  Typography,
+  Chip,
+} from '@material-ui/core';
 import { useTranslation } from 'react-i18next';
 import { create, InstanceProps } from 'react-modal-promise';
 
@@ -7,11 +15,24 @@ import { ExampleData } from 'src/pages/Main/Examples/config';
 import { DefaultResolveFuncType } from 'src/components/Dialogs/types';
 import DialogTitle from 'src/components/Dialogs/DialogTitle';
 
-// const useStyles = makeStyles(() => ({
-//   img: {
-//     width: '100%',
-//   },
-// }));
+const useStyles = makeStyles(theme => ({
+  img: {
+    width: '50%',
+    borderRadius: 10,
+    marginRight: theme.spacing(4),
+  },
+  rightSide: {
+    '& > p': {
+      marginBottom: theme.spacing(1),
+    },
+  },
+  tagsContainer: {
+    '& > div': {
+      marginRight: theme.spacing(2),
+      marginBottom: theme.spacing(2),
+    },
+  },
+}));
 
 interface Props extends InstanceProps<DefaultResolveFuncType> {
   data: ExampleData;
@@ -19,8 +40,8 @@ interface Props extends InstanceProps<DefaultResolveFuncType> {
 
 const PopUp: FC<Props> = ({ data, onReject }) => {
   const [t] = useTranslation();
-  // const classes = useStyles();
-  const { nameKey } = data;
+  const classes = useStyles();
+  const { nameKey, descKey, imgPath, year, customer, tags, urls } = data;
 
   const handleClose = useCallback(() => {
     onReject();
@@ -31,7 +52,35 @@ const PopUp: FC<Props> = ({ data, onReject }) => {
       <DialogTitle title={t(nameKey)} onClose={handleClose} />
 
       <DialogContent>
-        <pre>{JSON.stringify(data, null, 2)}</pre>
+        <Box display="flex" mb={4}>
+          <img src={imgPath} alt={t(nameKey)} className={classes.img} />
+
+          <Box className={classes.rightSide}>
+            <Typography variant="body1">{t(descKey)}</Typography>
+            <Typography variant="body1">
+              <strong>{capitalize(t('year'))}:</strong> {year}
+            </Typography>
+            <Typography variant="body1">
+              <strong>{capitalize(t('customer'))}:</strong> {customer}
+            </Typography>
+
+            {/* demo links */}
+            <Box display="flex" gridGap={10} mt={2}>
+              {urls.map((url, urlInd) => (
+                <a target="_blank" href={url} rel="noreferrer" key={url}>
+                  {t('examplePopUp__demoLabel')} {urlInd + 1}
+                </a>
+              ))}
+            </Box>
+          </Box>
+        </Box>
+
+        {/* tags */}
+        <Box mb={2} className={classes.tagsContainer}>
+          {tags.map(tag => (
+            <Chip key={tag} label={tag} />
+          ))}
+        </Box>
       </DialogContent>
     </Dialog>
   );
