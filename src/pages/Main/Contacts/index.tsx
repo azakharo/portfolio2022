d92478205315949b68d9be848cec38a4936536d9
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2022-10-15 14:46:22 +0300

    implemented the contacts section

--- src/pages/Main/Contacts/index.tsx
+++ src/pages/Main/Contacts/index.tsx
@@ -1,20 +1,80 @@
 import React, { FC, memo } from 'react';
-import { Box, makeStyles } from '@material-ui/core';
-import { SECTION__CONTACTS } from 'src/pages/Main/sectionIds';
+import { Box, makeStyles, Typography } from '@material-ui/core';
+import { useTranslation } from 'react-i18next';
 
-const useStyles = makeStyles(theme => ({
+import { SECTION__CONTACTS } from 'src/pages/Main/sectionIds';
+import { email, location, phone, resumeUrl } from 'src/config';
+
+const useStyles = makeStyles(() => ({
   root: {
-    minHeight: 150,
-    backgroundColor: theme.palette.action.focus,
+    backgroundColor: '#fafafa',
   },
 }));
 
 const Contacts: FC = () => {
+  const [t] = useTranslation();
   const classes = useStyles();
 
   return (
-    <Box id={SECTION__CONTACTS} className={classes.root}>
-      Contacts
+    <Box
+      id={SECTION__CONTACTS}
+      className={classes.root}
+      py={3}
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      flexWrap="wrap"
+      gridGap={50}
+    >
+      {/* Title */}
+      <Box mr={10}>
+        <Typography variant="h2">{t('contacts__title')}</Typography>
+        <Box textAlign="right">
+          <Typography variant="h5" color="textSecondary">
+            {t('contacts__subTitle')}
+          </Typography>
+        </Box>
+      </Box>
+
+      {/* Location */}
+      <Box>
+        <Typography variant="h5">{t('contacts__location__label')}</Typography>
+        <Typography variant="body2">
+          <a target="_blank" href={location} rel="noreferrer">
+            {t('contacts__location__value')}
+          </a>
+        </Typography>
+      </Box>
+
+      {/* Phone */}
+      <Box>
+        <Typography variant="h5">{t('contacts__phone__label')}</Typography>
+        <Typography variant="body2">
+          <a target="_blank" href={`tel:${phone}`} rel="noreferrer">
+            {phone}
+          </a>
+        </Typography>
+      </Box>
+
+      {/* Email */}
+      <Box>
+        <Typography variant="h5">{t('contacts__email__label')}</Typography>
+        <Typography variant="body2">
+          <a target="_blank" href={`mailto:${email}`} rel="noreferrer">
+            {email}
+          </a>
+        </Typography>
+      </Box>
+
+      {/* Resume */}
+      <Box>
+        <Typography variant="h5">{t('contacts__resume__label')}</Typography>
+        <Typography variant="body2">
+          <a target="_blank" href={resumeUrl} rel="noreferrer">
+            hh.ru
+          </a>
+        </Typography>
+      </Box>
     </Box>
   );
 };
