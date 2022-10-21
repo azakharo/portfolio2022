c9d0fb5bbb9a5dc47a0352ea4263cc4865c5290d
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2022-10-21 17:18:40 +0300

    skills: play the animation only when visible

--- src/pages/Main/Skills/Progress.tsx
+++ src/pages/Main/Skills/Progress.tsx
@@ -1,7 +1,8 @@
 import random from 'lodash/random';
-import React, { FC, memo, ReactNode, useMemo, useState } from 'react';
+import React, { FC, memo, ReactNode, useMemo, useRef, useState } from 'react';
 import { Box, LinearProgress, makeStyles } from '@material-ui/core';
 import useInterval from 'ahooks/lib/useInterval';
+import useInViewport from 'ahooks/lib/useInViewport';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -23,20 +24,27 @@ interface Props {
 
 const Progress: FC<Props> = ({ label, value, icon }) => {
   const classes = useStyles();
+  const containerRef = useRef(null);
+  const [isVisible] = useInViewport(containerRef, {
+    threshold: 1,
+  });
   const interval = useMemo(() => random(1, 5, false) * 10, []); // ms
 
   const [currentValue, setCurrentValue] = useState(0);
-  const clearInterval = useInterval(() => {
-    if (currentValue < value) {
-      setCurrentValue(currentValue + 1);
-    } else {
-      clearInterval();
-    }
-  }, interval);
+  const clearInterval = useInterval(
+    () => {
+      if (currentValue < value) {
+        setCurrentValue(currentValue + 1);
+      } else {
+        clearInterval();
+      }
+    },
+    isVisible ? interval : undefined,
+  );
 
   // This component repeats the layout of the About comp.
   return (
-    <>
+    <div ref={containerRef}>
       <Box mb={1} display="flex" justifyContent="space-between">
         <Box display="flex" alignItems="center">
           {icon && <>{icon}&nbsp;</>}
@@ -52,7 +60,7 @@ const Progress: FC<Props> = ({ label, value, icon }) => {
           bar: classes.bar,
         }}
       />
-    </>
+    </div>
   );
 };
 
