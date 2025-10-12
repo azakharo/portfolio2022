b4c77ab5708c8aa7107535182dea59e4847a2319
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2025-10-12 17:43:45 +0300

    impl header

--- src/pages/ExamplePage/index.tsx
+++ src/pages/ExamplePage/index.tsx
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { Helmet } from 'react-helmet';
+import { Box, IconButton, Typography } from '@material-ui/core';
+import { useParams, useHistory } from 'react-router-dom';
+import { examples } from 'src/pages/Main/Examples/config';
+import { useTranslation } from 'react-i18next';
+import ArrowBackOutlinedIcon from '@material-ui/icons/ArrowBackOutlined';
 
-export const ExamplePage = () => (
-  <>
-    <Helmet>
-      <title>azakharov</title>
-    </Helmet>
+export const ExamplePage: FC = () => {
+  const [t] = useTranslation();
+  const history = useHistory();
+  const { id } = useParams<{ id: string }>();
+  const example = examples.find(ex => ex.id === id);
 
-    <div>ha-ha-ha</div>
-  </>
-);
+  if (!example) {
+    return null;
+  }
+
+  const { nameKey } = example;
+
+  return (
+    <>
+      <Helmet>
+        <title>azakharov</title>
+      </Helmet>
+
+      <Box
+        component="header"
+        display="flex"
+        alignItems="center"
+        px={2}
+        py={1}
+        bgcolor="#F6F6F6"
+      >
+        <IconButton
+          onClick={() => {
+            history.goBack();
+          }}
+        >
+          <ArrowBackOutlinedIcon style={{ color: '#000' }} />
+        </IconButton>
+
+        <Typography variant="h4">{t(nameKey)}</Typography>
+      </Box>
+    </>
+  );
+};
