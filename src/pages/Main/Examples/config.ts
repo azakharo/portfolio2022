da098d08df3c96f645b652b64972ac83fca45be7
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2025-10-15 10:11:42 +0300

    added yon example

--- src/pages/Main/Examples/config.ts
+++ src/pages/Main/Examples/config.ts
@@ -11,7 +11,6 @@ import detectorImg from 'src/assets/examples/detector_ui.png';
 import co2Img from 'src/assets/examples/co2.png';
 import safeCityImg from 'src/assets/examples/safeCity.jpg';
 import votumImg from 'src/assets/examples/votum.png';
-import asrImg from 'src/assets/examples/asr.png';
 import imgIz from 'src/assets/examples/iz/iz.jpg';
 import imgRen from 'src/assets/examples/iz/ren.jpg';
 import imgFos from 'src/assets/examples/iz/fos.jpg';
@@ -32,8 +31,39 @@ import imgNovakid4 from 'src/assets/examples/novakid/demo4.png';
 import imgNovakid5 from 'src/assets/examples/novakid/demo5.png';
 import imgNovakid6 from 'src/assets/examples/novakid/demo6.png';
 import imgNovakid7 from 'src/assets/examples/novakid/demo7.png';
+import imgYonLogo from 'src/assets/examples/yon/logo.jpg';
+import imgYon1 from 'src/assets/examples/yon/photo1.jpg';
+import imgYon2 from 'src/assets/examples/yon/photo2.jpg';
+import imgYon3 from 'src/assets/examples/yon/photo3.jpg';
+import imgYon4 from 'src/assets/examples/yon/photo4.jpg';
 
 export const examples: ExampleData[] = [
+  {
+    id: 'yon',
+    nameKey: 'examples__yon__name',
+    imgPath: imgYonLogo,
+    demoImageUrls: [imgYon1, imgYon2, imgYon3, imgYon4],
+    descKey: 'examples__yon__description',
+    tags: [
+      'React.js',
+      'Typescript',
+      'MUI',
+      'react-query',
+      'i18next',
+      'storybook',
+      'vite',
+      'vitest',
+      'react-testing-library',
+      'FSD',
+      'eslint',
+      'prettier',
+      'axios',
+      'date-fns',
+      'react-hook-form + yup',
+    ],
+    year: 2025,
+    customerKey: 'examples__yon__customer',
+  },
   {
     id: 'lenta',
     nameKey: 'examples__lenta__name',
@@ -378,14 +408,14 @@ export const examples: ExampleData[] = [
     year: 2011,
     customer: 'ЦИК РФ',
   },
-  {
-    id: 'asr',
-    nameKey: 'examples__asr__name',
-    imgPath: asrImg,
-    demoImageUrls: [],
-    descKey: 'examples__asr__description',
-    tags: ['Django', 'Python'],
-    year: 2011,
-    customer: 'ИТЦ Система-Саров',
-  },
+  // {
+  //   id: 'asr',
+  //   nameKey: 'examples__asr__name',
+  //   imgPath: asrImg,
+  //   demoImageUrls: [],
+  //   descKey: 'examples__asr__description',
+  //   tags: ['Django', 'Python'],
+  //   year: 2011,
+  //   customer: 'ИТЦ Система-Саров',
+  // },
 ];
